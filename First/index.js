309bc87a31aa64a077c922756df5e26b0af8ca94
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-11 12:27:46 +0530

    Add .gitignore and update package.json with express dependency; refactor server to use Express

--- First/index.js
+++ First/index.js
@@ -1,6 +1,14 @@
 // Creating Server Http Server
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("Hello, world!");
-});
-server.listen(3000);
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.end("Hello, world!");
+// });
+// server.listen(3000);
+
+// Creating Server in Express Server
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("Hello, world!");
+})
+app.listen(3000);
