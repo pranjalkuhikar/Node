52814e0ee877ffbbb670023a65422e1af0994f1b
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-11 12:41:31 +0530

    Form Handling

--- First/index.js
+++ First/index.js
@@ -14,14 +14,33 @@
 // app.listen(3000);
 
 // Creating SSR through EJS
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+// app.listen(3000);
+
+// Form Handling
 const express = require("express");
 const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  res.send("Received");
 });
 app.listen(3000);
