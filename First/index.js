5258a6c66de24dd6fa31b4edb986af6bf4b02365
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-11 12:34:48 +0530

    Implement server-side rendering with EJS; add static files and update package.json for EJS dependency

--- First/index.js
+++ First/index.js
@@ -6,9 +6,22 @@
 // server.listen(3000);
 
 // Creating Server in Express Server
+// const express = require("express");
+// const app = express();
+// app.get("/",(req,res)=>{
+//     res.send("Hello, world!");
+// })
+// app.listen(3000);
+
+// Creating SSR through EJS
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("Hello, world!");
-})
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 app.listen(3000);
