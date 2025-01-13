5b3ede3c504d174b8a052302b2a211d743dfc941
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-13 11:22:14 +0530

    Comment out server setup and add 404 error handling route

--- First/index.js
+++ First/index.js
@@ -14,19 +14,24 @@
 // app.listen(3000);
 
 // Creating SSR through EJS
-const express = require("express");
-const app = express();
-const path = require("path");
-const morgan = require("morgan");
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const morgan = require("morgan");
 
-app.use(morgan("dev"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(morgan("dev"));
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.listen(3000);
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("*", (req, res) => {
+//   res.send("404 Page Not Found");
+// });
+
+// app.listen(3000);
 
 // Form Handling
 // const express = require("express");
