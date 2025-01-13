ba049cf18b47da142bff2664a61696b3eb9f1100
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-13 11:17:09 +0530

    Add Morgan middleware for logging and update package.json for new dependency; modify CSS for header color

--- First/index.js
+++ First/index.js
@@ -14,33 +14,35 @@
 // app.listen(3000);
 
 // Creating SSR through EJS
+const express = require("express");
+const app = express();
+const path = require("path");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(3000);
+
+// Form Handling
 // const express = require("express");
 // const app = express();
 // const path = require("path");
 
 // app.set("view engine", "ejs");
 // app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.render("index");
 // });
+// app.post("/create", (req, res) => {
+//   console.log(req.body);
+//   res.send("Received");
+// });
 // app.listen(3000);
-
-// Form Handling
-const express = require("express");
-const app = express();
-const path = require("path");
-
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.post("/create", (req, res) => {
-  console.log(req.body);
-  res.send("Received");
-});
-app.listen(3000);
