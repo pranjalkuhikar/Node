54eea8d92043025cc2a3bc5f04300dc450195b4a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-01-24 11:32:37 +0530

    Refactor routing to use separate index routes and controller

--- Project/Social_LikeCount/src/app.js
+++ Project/Social_LikeCount/src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
-
+import indexRoutes from "./routes/index.routes.js";
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.use("/", indexRoutes);
 
 export default app;
